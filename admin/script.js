54075f38d309763d4e374a6a2ae4bc1cf83ab11c
Suryo Saputro <suryo237658@student.smkn1kandeman.sch.id>
2024-11-22 08:54:32 +0700

    Update script.js

--- admin/script.js
+++ admin/script.js
@@ -1,15 +1,46 @@
 const username = "admin703@gmail.com"
 const password = "Admin703"
 
+
+const tutupGagal = () => {
+    // mengubah posisi toast ketika tombol ditekan
+    document.getElementById("toastGagal").style.top = "-50px"
+}
+
+const tutupBerhasil = () => {
+    // mengubah posisi toast ketika tombol ditekan
+    document.getElementById("toastBerhasil").style.top = "-50px"
+    window.location.href = "dashboard.html"
+}
+
 const lgn = () => {
     const user = document.getElementById("usr").value
     const pass = document.getElementById("pswd").value
     if (user === username && pass === password) {
-        alert("Login Success")
-        window.location.href = "dashboard.html"
+        document.getElementById("toastBerhasil").style.top = "5px"
     } else {
-        alert("Login Gagal!")
+        document.getElementById("toastGagal").style.top = "5px"
         document.getElementById("usr").value = ""
         document.getElementById("pswd").value = ""
     }
+}
+
+const showTemplate = () => {
+    document.getElementById("template").style.top = "72px"
+    document.getElementById("user").style.top = "-1000px"
+    document.getElementById("profile").style.top = "-1000px"
+}
+const showUser = () => {
+    document.getElementById("user").style.top = "72px"
+    document.getElementById("template").style.top = "-1000px"
+    document.getElementById("profile").style.top = "-1000px"
+}
+const showProfile = () => {
+    document.getElementById("profile").style.top = "72px"
+    document.getElementById("template").style.top = "-1000px"
+    document.getElementById("user").style.top = "-1000px"
+}
+
+const logout = () => {
+    window.location.href = "login.html"
 }
